09e58f69854b1c93102c1e84efb6c495d80f43b8
Stefan Staynov <stefan.staynov@mentormate.com>
2024-09-05 16:33:22 +0300

    Add an example for translating plurals

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,10 +12,19 @@ export class HomeComponent {
 
   today = new Date();
   message = $localize`:@@customMessage:This is a message coming from the component itself!`;
+  itemCount = 0;
 
   changeLanguage(locale: string): void {
     localStorage.setItem('locale', locale);
     window.location.reload();
+  }
+
+  clearItemCount(): void {
+    this.itemCount = 0;
+  }
+
+  incrementItemCount(): void {
+    this.itemCount++;
   }
 
 }
