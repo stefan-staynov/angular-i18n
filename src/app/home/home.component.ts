b6b6f7cbb772e1e8e62bdaf4d1ccd2c4ee6135b4
Stefan Staynov <stefan.staynov@mentormate.com>
2024-09-05 13:48:32 +0300

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  today = new Date();
+  message = $localize`This is a message coming from the component itself!`;
+
+}
